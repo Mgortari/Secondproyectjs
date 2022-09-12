2561de7511ab3315845cad93c03b6c944e249ab7
M Gortari <marcogortari.7@gmail.com>
2022-09-11 23:58:41 -0300

    Second proyect js

---
+++ main.js
@@ -0,0 +1,36 @@
+let bebidas = ['coca', "pepsi", "seven up", "gatorade", "ades", "fernet"];
+
+let snack = ["papas", "palitos", "queso", "mani", "kesitos"];
+
+const part = bebidas.concat(snack);
+
+part.forEach(function(i){
+    alert(`Listado para fiesta\n ${i}`)
+});
+
+
+
+function showMenu(){
+    this.i = (prompt(`Falta algo mas para llevar?`))
+
+    return showMenu
+
+}
+
+showMenu();
+
+function show(showMenu){
+    alert (`Usted eligio llevar ${i}`)
+    return show
+}
+
+show();
+
+
+
+console.log(bebidas)
+console.log(snack)
+bebidas.push("beer")
+snack.pop()
+console.log(i)
+
